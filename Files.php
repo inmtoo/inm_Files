73f1c849398b41201e7e4d9e1ad2baebb8db7f8e
inmtoo <inmtoo@yandex.ru>
2019-04-10 19:06:57 +0400

    No changes, add author info

--- Files.php
+++ Files.php
@@ -1,30 +1,8 @@
 <?php
     class Files {
 		/*
-        public function move_to ($source, $destination, $random_rename) {
-            
-        }
-        
-        public function copy_from_site_to ($source, $destination, $random_rename) {
-            $extension = Files::getExtension($source);
-            $basename = basename($source, '.'.$extension);
-            if ( $random_rename == 1 ) {
-                $newname = md5($basename.date("Y-m-d").date("H:i:s"));
-            } else {
-                $newname = $basename;
-            }
-            
-            if (copy($source, $destination.$newname.'.'.$extension)) {
-               // return $destination.$newname.'.'.$extension;
-               return str_replace("/home/m/myzaprosru/sp-kubani/public_html", "", $destination.$newname.'.'.$extension);
-            } else {
-                return 'Error';
-            }
-        }
-        
-        public function getExtension($filename) {
-            return end(explode(".", $filename));
-        }
+       			Author: Inmtoo.com Max Sharun & Vlat Serednyakov
+			Web Development: http://inmtoo.com
 		*/
 		public function CheckFiles($files, $numFiles = 10, $allowext = array("jpg", "png", "gif", "txt", "doc", "csv"), $disallowext = array("php", "html", "sh", "js", "cmd", ".htaccess"))
 		{
